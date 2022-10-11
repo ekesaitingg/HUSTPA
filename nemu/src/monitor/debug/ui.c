86873f0e5812200247599221a417db6163ce0f6d
tracer-ics2019 <tracer@njuics.org>
2022-10-11 20:45:25 +0800

    >  compile U201914983 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:45:25 up 19:56,  1 user,  load average: 0.33, 0.20, 0.12 93c4e0da50f5cbc112efeadd3c4e60bb1a55b1fb

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -160,7 +160,7 @@ static int cmd_x(char *args)
   for (i = 0; i < N; i++)
   {
     /*TODO: paddr_read ? vaddr_read? I think it should be vaddr_read! */
-    printf("0x%08x \t\t 0x%08x \n", EXPR, vaddr_read(EXPR, 4)); /* len = 4  */
+    printf("0x%08x \t 0x%08x \n", EXPR, vaddr_read(EXPR, 4)); /* len = 4  */
     EXPR += 4;
   }
   return 0;
