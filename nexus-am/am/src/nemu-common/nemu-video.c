399f9516c3d38da9ce50b6356e8253fd8d323861
tracer-ics2019 <tracer@njuics.org>
2022-10-15 17:09:35 +0800

    >  compile U201914983 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:09:35 up 14:21,  1 user,  load average: 0.26, 0.14, 0.14 5df5a994a179835385ce1ecb7761ab2a55de9466

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -2,31 +2,56 @@
 #include <amdev.h>
 #include <nemu.h>
 
-size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
-  switch (reg) {
-    case _DEVREG_VIDEO_INFO: {
-      _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      info->width = 0;
-      info->height = 0;
-      return sizeof(_DEV_VIDEO_INFO_t);
-    }
+size_t __am_video_read(uintptr_t reg, void *buf, size_t size)
+{
+  switch (reg)
+  {
+  case _DEVREG_VIDEO_INFO:
+  {
+    _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
+    info->width = 400;
+    info->height = 300;
+    return sizeof(_DEV_VIDEO_INFO_t);
+  }
   }
   return 0;
 }
 
-size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
-  switch (reg) {
-    case _DEVREG_VIDEO_FBCTL: {
-      _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-
-      if (ctl->sync) {
-        outl(SYNC_ADDR, 0);
+size_t __am_video_write(uintptr_t reg, void *buf, size_t size)
+{
+  switch (reg)
+  {
+  case _DEVREG_VIDEO_FBCTL:
+  {
+    _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
+    int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+    int W = screen_width();
+    int H = screen_height();
+    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+    uint32_t *pixels = ctl->pixels;
+    for (int i = 0; i < h; i++)
+    {
+      for (int j = 0; j < w; j++)
+      {
+        fb[(y + i) * W + x + j] = pixels[i * w + j];
       }
-      return size;
     }
+    if (ctl->sync)
+    {
+      outl(SYNC_ADDR, 0);
+    }
+    return size;
+  }
   }
   return 0;
 }
 
-void __am_vga_init() {
+void __am_vga_init()
+{
+  int i;
+  int size = screen_width() * screen_height();
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < size; i++)
+    fb[i] = 0;
+  draw_sync();
 }
