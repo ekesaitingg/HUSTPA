2888e42002304e3ecadbc928a5a3872ddf1a0404
tracer-ics2019 <tracer@njuics.org>
2022-10-15 19:29:49 +0800

    >  run U201914983 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:29:49 up 16:08,  1 user,  load average: 0.31, 0.13, 0.08 98bb2bd6a07c426db04f0853afd88e1bfe5226fa

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -4,14 +4,17 @@
 
 #define KEYDOWN_MASK 0x8000
 
-size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
-  switch (reg) {
-    case _DEVREG_INPUT_KBD: {
-      _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-      kbd->keydown = 0;
-      kbd->keycode = _KEY_NONE;
-      return sizeof(_DEV_INPUT_KBD_t);
-    }
+size_t __am_input_read(uintptr_t reg, void *buf, size_t size)
+{
+  switch (reg)
+  {
+  case _DEVREG_INPUT_KBD:
+  {
+    _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
+    kbd->keydown = (KBD_ADDR & KEYDOWN_MASK ? 1 : 0);
+    kbd->keycode = inl(KBD_ADDR);
+    return sizeof(_DEV_INPUT_KBD_t);
+  }
   }
   return 0;
 }
