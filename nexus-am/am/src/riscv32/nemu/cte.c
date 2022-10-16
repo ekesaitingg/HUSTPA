3f98167625616a963e240dad12a995b8cc3db725
tracer-ics2019 <tracer@njuics.org>
2022-10-16 16:19:49 +0800

    >  compile U201914983 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:19:49 up 20:08,  1 user,  load average: 0.38, 0.17, 0.14 80a574c3d007e8716de73c0b96b4376e9363150f

--- nexus-am/am/src/riscv32/nemu/cte.c
+++ nexus-am/am/src/riscv32/nemu/cte.c
@@ -1,18 +1,38 @@
 #include <am.h>
 #include <riscv32.h>
 
-static _Context* (*user_handler)(_Event, _Context*) = NULL;
+static _Context *(*user_handler)(_Event, _Context *) = NULL;
 
-_Context* __am_irq_handle(_Context *c) {
+_Context *__am_irq_handle(_Context *c)
+{
   _Context *next = c;
-  if (user_handler) {
+  if (user_handler)
+  {
     _Event ev = {0};
-    switch (c->cause) {
-      default: ev.event = _EVENT_ERROR; break;
+    switch (c->cause)
+    {
+    case -1:
+      ev.event = _EVENT_YIELD;
+      break;
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 7:
+    case 8:
+    case 9:
+    case 13:
+      ev.event = _EVENT_SYSCALL;
+      break;
+    default:
+      ev.event = _EVENT_ERROR;
+      break;
     }
 
     next = user_handler(ev, c);
-    if (next == NULL) {
+    if (next == NULL)
+    {
       next = c;
     }
   }
@@ -22,9 +42,12 @@ _Context* __am_irq_handle(_Context *c) {
 
 extern void __am_asm_trap(void);
 
-int _cte_init(_Context*(*handler)(_Event, _Context*)) {
+int _cte_init(_Context *(*handler)(_Event, _Context *))
+{
   // initialize exception entry
-  asm volatile("csrw stvec, %0" : : "r"(__am_asm_trap));
+  asm volatile("csrw stvec, %0"
+               :
+               : "r"(__am_asm_trap));
 
   // register event handler
   user_handler = handler;
@@ -32,17 +55,21 @@ int _cte_init(_Context*(*handler)(_Event, _Context*)) {
   return 0;
 }
 
-_Context *_kcontext(_Area stack, void (*entry)(void *), void *arg) {
+_Context *_kcontext(_Area stack, void (*entry)(void *), void *arg)
+{
   return NULL;
 }
 
-void _yield() {
+void _yield()
+{
   asm volatile("li a7, -1; ecall");
 }
 
-int _intr_read() {
+int _intr_read()
+{
   return 0;
 }
 
-void _intr_write(int enable) {
+void _intr_write(int enable)
+{
 }
