127eb919eb418d3432ff918cb28e72a72b21cafa
tracer-ics2019 <tracer@njuics.org>
2022-10-17 21:19:37 +0800

    >  run U201914983 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:19:37 up 1 day,  6:45,  1 user,  load average: 0.24, 0.12, 0.20 d4a7c979107bc0f8c6e29d3e4c968f5f7987f513

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -9,8 +9,9 @@ void init_proc(void);
 
 extern const char logo[];
 
-int main() {
-  printf("%s", logo);
+int main()
+{
+  // printf("%s", logo);
   Log("'Hello World!' from Nanos-lite");
   Log("Build time: %s, %s", __TIME__, __DATE__);
 
