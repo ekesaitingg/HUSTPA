8020e0e1d55bbca1553a4a0ffcc38d7e8fd337e9
tracer-ics2019 <tracer@njuics.org>
2022-10-17 12:11:52 +0800

    >  run U201914983 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:11:52 up 1 day,  1:02,  1 user,  load average: 0.13, 0.18, 0.18 18de2d34a08d85d0d5dd460979aae363f1050e62

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -36,6 +36,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)
 
 void naive_uload(PCB *pcb, const char *filename)
 {
+  printf("prepare to load program %s", filename);
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %x", entry);
   ((void (*)())entry)();
