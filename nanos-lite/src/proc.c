27962bf2c0435f38a0d6a4b96f3312ef941952f2
tracer-ics2019 <tracer@njuics.org>
2022-10-17 16:26:52 +0800

    >  gdb U201914983 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:26:52 up 1 day,  1:53,  1 user,  load average: 0.32, 0.21, 0.12 c4ea556bf1c502be88acd439bda693303834be

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,7 +29,7 @@ void init_proc()
 
   Log("Initializing processes...");
   printf("prepare to load program.\n");
-  naive_uload(NULL, NULL);
+  naive_uload(NULL, "/bin/dummy");
   //   load program here
 }
 
