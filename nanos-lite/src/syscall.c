c7bb067a22b25bd6f412be4d1cb9f0d0c771fde1
tracer-ics2019 <tracer@njuics.org>
2022-10-17 16:49:39 +0800

    >  run U201914983 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:49:39 up 1 day,  2:16,  1 user,  load average: 0.10, 0.17, 0.17 d79e376ce4b416f54d8e62c75293b1f6ff7431c6

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,28 +12,28 @@ _Context *do_syscall(_Context *c)
   printf("syscall %d reached.\n", a[0]);
   switch (a[0])
   {
-  case SYS_exit:
+  case SYS_exit: // syscall0
     _halt(a[1]);
     break;
-  case SYS_yield:
+  case SYS_yield: // syscall1
     _yield();
     break;
-  case SYS_write:
-    c->GPRx = fs_write(a[1], (void *)a[2], a[3]);
-    break;
-  case SYS_read:
-    c->GPRx = fs_read(a[1], (void *)a[2], a[3]);
-    break;
-  case SYS_open:
+  case SYS_open: // syscall2
     c->GPRx = fs_open((char *)a[1], a[2], a[3]);
     break;
-  case SYS_close:
+  case SYS_read: // syscall3
+    c->GPRx = fs_read(a[1], (void *)a[2], a[3]);
+    break;
+  case SYS_write: // syscall4
+    c->GPRx = fs_write(a[1], (void *)a[2], a[3]);
+    break;
+  case SYS_close: // syscall7
     c->GPRx = fs_close(a[1]);
     break;
-  case SYS_lseek:
+  case SYS_lseek: // syscall8
     c->GPRx = fs_lseek(a[1], a[2], a[3]);
     break;
-  case SYS_brk:
+  case SYS_brk: // syscall9
     break;
   default:
     panic("Unhandled syscall ID = %d", a[0]);
