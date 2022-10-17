7573cbf89f1d3bd68c44653f35f0f060bd34aaa8
tracer-ics2019 <tracer@njuics.org>
2022-10-17 12:08:04 +0800

    >  run U201914983 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:08:04 up 1 day, 58 min,  1 user,  load average: 0.03, 0.15, 0.19 847f1b7a2b1ba2a5e23d9428ea63ed0fd78aa264

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -2,6 +2,7 @@
 
 #define MAX_NR_PROC 4
 
+extern void naive_uload(PCB *, const char *);
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
 PCB *current = NULL;
@@ -27,7 +28,7 @@ void init_proc()
   switch_boot_pcb();
 
   Log("Initializing processes...");
-  // naive_uload(NULL, NULL);
+  naive_uload(NULL, NULL);
   //   load program here
 }
 
