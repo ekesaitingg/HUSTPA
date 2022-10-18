4ed14e69dbc2ed8e56c0c283183b78fb36bdc875
tracer-ics2019 <tracer@njuics.org>
2022-10-18 21:29:20 +0800

    >  run U201914983 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:29:20 up  4:12,  1 user,  load average: 0.39, 0.54, 0.59 3e8c2b5762386ccd05043813044b8f4a5506cc

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -152,11 +152,5 @@ int fs_close(int fd)
 void init_fs()
 {
   // TODO: initialize the size of /dev/fb
-  int fb = fs_open("/dev/fb", 0, 0);
-  file_table[fb].size = screen_width() * screen_height() * sizeof(uint32_t);
-  fb = fs_open("/dev/fbsync", 0, 0);
-  file_table[fb].size = 1;
-  extern size_t get_dispinfo_size();
-  fb = fs_open("/proc/dispinfo", 0, 0);
-  file_table[fb].size = get_dispinfo_size();
+  file_table[FD_FB].size = screen_width() * screen_height() * 4;
 }
