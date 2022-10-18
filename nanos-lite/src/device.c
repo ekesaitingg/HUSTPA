c23b4c785aa039fe6b65fe069265945e074de51d
tracer-ics2019 <tracer@njuics.org>
2022-10-18 21:31:30 +0800

    >  run U201914983 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:31:30 up  4:14,  1 user,  load average: 0.17, 0.43, 0.54 b152da089d97d8079f71aba4e8d64fddc6ad685

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -3,7 +3,7 @@
 
 size_t serial_write(const void *buf, size_t offset, size_t len)
 {
-  for (size_t i = 0; i < len; i++)
+  for (size_t i = 0; i < len; ++i)
     _putc(((char *)buf)[i]);
   return len;
 }
@@ -17,21 +17,22 @@ static const char *keyname[256] __attribute__((used)) = {
 
 size_t events_read(void *buf, size_t offset, size_t len)
 {
-  int key = read_key();
-  int down = 0;
-  if (key & 0x8000)
+  int keycode = read_key();
+  if (keycode != _KEY_NONE)
   {
-    key ^= 0x8000;
-    down = 1;
-  }
-  if (key != _KEY_NONE)
-  {
-    len = sprintf(buf, "%s %s\n", down ? "kd" : "ku", keyname[key]);
+    if (keycode & 0x8000)
+    {
+      keycode ^= 0x8000;
+      len = sprintf(buf, "kd %s\n", keyname[keycode]);
+    }
+    else if (!((keycode & ~0x8000) == _KEY_NONE))
+    {
+      len = sprintf(buf, "ku %s\n", keyname[keycode]);
+    }
   }
   else
   {
-    int time = uptime();
-    len = sprintf(buf, "t %d\n", time);
+    len = sprintf(buf, "t %u\n", uptime());
   }
   return len;
 }
