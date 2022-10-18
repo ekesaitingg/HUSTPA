9e8930cee59ee924a2f68ddc5decedf0cea9dc09
tracer-ics2019 <tracer@njuics.org>
2022-10-18 11:00:16 +0800

    >  run U201914983 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:00:16 up 34 min,  1 user,  load average: 0.26, 0.20, 0.18 47e8f37dc593d2c68c208b6635011a73dd6776a2

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <assert.h>
 
-int main() {
+int main()
+{
+  printf("text: try to open file.\n");
   FILE *fp = fopen("/share/texts/num", "r+");
   printf("main: fp = ")
-  assert(fp);
+      assert(fp);
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
@@ -12,23 +14,27 @@ int main() {
 
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
-  for (i = 500; i < 1000; i ++) {
+  for (i = 500; i < 1000; i++)
+  {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
 
   fseek(fp, 0, SEEK_SET);
-  for (i = 0; i < 500; i ++) {
+  for (i = 0; i < 500; i++)
+  {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
 
-  for (i = 500; i < 1000; i ++) {
+  for (i = 500; i < 1000; i++)
+  {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
 
   fseek(fp, 0, SEEK_SET);
-  for (i = 0; i < 500; i ++) {
+  for (i = 0; i < 500; i++)
+  {
     fscanf(fp, "%d", &n);
     assert(n == i + 1 + 1000);
   }
