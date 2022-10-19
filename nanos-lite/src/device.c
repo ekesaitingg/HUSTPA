dc8d58a206ffc8857e8a6940f6a5ccd952f96359
tracer-ics2019 <tracer@njuics.org>
2022-10-19 09:57:45 +0800

    >  run U201914983 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:57:45 up  4:42,  1 user,  load average: 0.37, 0.51, 0.55 82d45b114ca8b2043d0f1a79392ff7e75468a3a7

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,10 +1,9 @@
 #include "common.h"
 #include <amdev.h>
 
-// implement some function of device
 size_t serial_write(const void *buf, size_t offset, size_t len)
 {
-  for (int i = 0; i < len; i++)
+  for (size_t i = 0; i < len; ++i)
     _putc(((char *)buf)[i]);
   return len;
 }
@@ -39,6 +38,11 @@ size_t events_read(void *buf, size_t offset, size_t len)
 
 static char dispinfo[128] __attribute__((used)) = {};
 
+size_t get_dispinfo_size()
+{
+  return strlen(dispinfo);
+}
+
 size_t dispinfo_read(void *buf, size_t offset, size_t len)
 {
   len = sprintf(buf, dispinfo + offset);
@@ -49,19 +53,22 @@ size_t fb_write(const void *buf, size_t offset, size_t len)
 {
   int x = (offset / 4) % screen_width();
   int y = (offset / 4) / screen_width();
-  draw_rect((uint32_t *)buf, x, y, len / 4, 1);
+  draw_rect((void *)buf, x, y, len / 4, 1);
   return len;
 }
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len)
 {
   draw_sync();
-  return len;
+  return 0;
 }
 
 void init_device()
 {
   Log("Initializing devices...");
   _ioe_init();
+
+  // TODO: print the string to array `dispinfo` with the format
+  // described in the Navy-apps convention
   sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", screen_width(), screen_height());
 }
