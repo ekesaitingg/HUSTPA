b3032f098e837f8f51d601699e08695088dadf8c
tracer-ics2019 <tracer@njuics.org>
2022-10-19 09:56:50 +0800

    >  run U201914983 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:56:50 up  4:41,  1 user,  load average: 0.58, 0.58, 0.58 7edc41705c25178f5e06393f782f0b688b196d

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,9 +1,10 @@
 #include "common.h"
 #include <amdev.h>
 
+// implement some function of device
 size_t serial_write(const void *buf, size_t offset, size_t len)
 {
-  for (size_t i = 0; i < len; ++i)
+  for (int i = 0; i < len; i++)
     _putc(((char *)buf)[i]);
   return len;
 }
@@ -17,32 +18,26 @@ static const char *keyname[256] __attribute__((used)) = {
 
 size_t events_read(void *buf, size_t offset, size_t len)
 {
-  int keycode = read_key();
-  if (keycode != _KEY_NONE)
+  int key = read_key();
+  int down = 0;
+  if (key & 0x8000)
   {
-    if (keycode & 0x8000)
-    {
-      keycode ^= 0x8000;
-      len = sprintf(buf, "kd %s\n", keyname[keycode]);
-    }
-    else if (!((keycode & ~0x8000) == _KEY_NONE))
-    {
-      len = sprintf(buf, "ku %s\n", keyname[keycode]);
-    }
+    key ^= 0x8000;
+    down = 1;
+  }
+  if (key != _KEY_NONE)
+  {
+    len = sprintf(buf, "%s %s\n", down ? "kd" : "ku", keyname[key]);
   }
   else
   {
-    len = sprintf(buf, "t %u\n", uptime());
+    int time = uptime();
+    len = sprintf(buf, "t %d\n", time);
   }
   return len;
 }
 
 static char dispinfo[128] __attribute__((used)) = {};
-
-size_t get_dispinfo_size()
-{
-  return strlen(dispinfo);
-}
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len)
 {
@@ -54,22 +49,19 @@ size_t fb_write(const void *buf, size_t offset, size_t len)
 {
   int x = (offset / 4) % screen_width();
   int y = (offset / 4) / screen_width();
-  draw_rect((void *)buf, x, y, len / 4, 1);
+  draw_rect((uint32_t *)buf, x, y, len / 4, 1);
   return len;
 }
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len)
 {
   draw_sync();
-  return 0;
+  return len;
 }
 
 void init_device()
 {
   Log("Initializing devices...");
   _ioe_init();
-
-  // TODO: print the string to array `dispinfo` with the format
-  // described in the Navy-apps convention
   sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", screen_width(), screen_height());
 }
