21272185e353d5ac17096e19299532552f8c2c87
tracer-ics2019 <tracer@njuics.org>
2022-10-21 12:29:56 +0800

    >  run U201914983 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:29:56 up 18:07,  1 user,  load average: 0.27, 0.53, 0.53 c615a9026ae7eb84fb045669b79b7a9c8ea46a4b

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,7 +28,7 @@ void init_proc()
   switch_boot_pcb();
 
   Log("Initializing processes...");
-  naive_uload(NULL, "/bin/hello");
+  naive_uload(NULL, "/bin/evenets");
   //   load program here
 }
 
